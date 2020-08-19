41b1d17c0b84b76ee6579298b816b631c83c62f0
mariosfon <60343657+mariosfon@users.noreply.github.com>
2020-08-19 12:46:35 +0300

    commiting refactored function

--- src/components/day.js
+++ src/components/day.js
@@ -1,12 +1,34 @@
 import React from "react";
 import "./test";
 import "./city";
-
 const api = {
   key: "c919f4253df7ee10be22377a10066ac1",
   base: " https://api.openweathermap.org/data/2.5/",
 };
-
+async function getForecast(title) {
+  const req = await fetch(
+    `${api.base}forecast?q=${title}&units=metric&APPID=${api.key}`
+  );
+  const res = await req.json();
+  return {
+    activeCity: res.city.name,
+    activeWeather1: res.list[1].main.temp,
+    activeWeather2: res.list[9].main.temp,
+    activeWeather3: res.list[17].main.temp,
+    activeWeather4: res.list[25].main.temp,
+    activeWeather5: res.list[33].main.temp,
+    activeDesc1: res.list[1].weather[0].main,
+    activeDesc2: res.list[9].weather[0].main,
+    activeDesc3: res.list[17].weather[0].main,
+    activeDesc4: res.list[25].weather[0].main,
+    activeDesc5: res.list[33].weather[0].main,
+    icon1: res.list[0].weather[0].icon,
+    icon2: res.list[9].weather[0].icon,
+    icon3: res.list[17].weather[0].icon,
+    icon4: res.list[25].weather[0].icon,
+    icon5: res.list[33].weather[0].icon,
+  }
+}
 class Day extends React.Component {
   state = {
     activeCity: [],
@@ -15,33 +37,17 @@ class Day extends React.Component {
     sysName: [],
     icon: "",
   };
-
   componentDidMount = async () => {
     const title = this.props.location.state.weather;
-    const req = await fetch(
-      `${api.base}forecast?q=${title}&units=metric&APPID=${api.key}`
-    );
-
-    const res = await req.json();
-    this.setState({
-      activeCity: res.city.name,
-      activeWeather1: res.list[1].main.temp,
-      activeWeather2: res.list[9].main.temp,
-      activeWeather3: res.list[17].main.temp,
-      activeWeather4: res.list[25].main.temp,
-      activeWeather5: res.list[33].main.temp,
-
-      activeDesc1: res.list[1].weather[0].main,
-      activeDesc2: res.list[9].weather[0].main,
-      activeDesc3: res.list[17].weather[0].main,
-      activeDesc4: res.list[25].weather[0].main,
-      activeDesc5: res.list[33].weather[0].main,
-      icon1: res.list[0].weather[0].icon,
-      icon2: res.list[9].weather[0].icon,
-      icon3: res.list[17].weather[0].icon,
-      icon4: res.list[25].weather[0].icon,
-      icon5: res.list[33].weather[0].icon,
-    });
+    getForecast(title).then((values)=>{
+      console.log(values)
+      this.setState({
+        activeCity:values.activeCity,
+        activeWeather:[values.activeWeather1,values.activeWeather2,values.activeWeather3,values.activeWeather4,values.activeWeather5],
+        sysName: [values.activeDesc1,values.activeDesc2,values.activeDesc3,values.activeDesc4,values.activeDesc5],
+        icon:[values.icon1,values.icon2,values.icon3,values.icon4,values.icon5]
+      })
+    })
     console.log(this.state);
   };
   render() {
@@ -64,12 +70,9 @@ class Day extends React.Component {
         "Friday",
         "Saturday",
       ];
-
       let day = days[d.getDay()];
-
       return `${day} `;
     };
-
     return (
       <div
         className={
@@ -82,64 +85,60 @@ class Day extends React.Component {
       >
         <main>
           <div className="location"> {this.state.activeCity}</div>
-
           <div className="container">
             <div className="location-box"></div>
-
             <div className="weather-box">
               <div className="date">{dateBuilder(today)}</div>
               <div className="temp">
-                {Math.round(this.state.activeWeather1)} ‎°C{" "}
+                {Math.round(this.state.activeWeather[0])} ‎°C{" "}
               </div>
-              <div className="weather">{this.state.activeDesc1} </div>
+              <div className="weather">{this.state.sysName[0]} </div>
               <img
-                src={`http://openweathermap.org/img/wn/${this.state.icon1}.png`}
+                src={`http://openweathermap.org/img/wn/${this.state.icon[0]}.png`}
               />
             </div>
-
             <div className="location-box"></div>
             <div className="weather-box">
               <div className="date">{dateBuilder(tomorrow)}</div>
               <div className="temp">
-                {Math.round(this.state.activeWeather2)} ‎°C{" "}
+                {Math.round(this.state.activeWeather[1])} ‎°C{" "}
               </div>
-              <div className="weather">{this.state.activeDesc2} </div>
+              <div className="weather">{this.state.sysName[1]} </div>
               <img
-                src={`http://openweathermap.org/img/wn/${this.state.icon2}.png`}
+                src={`http://openweathermap.org/img/wn/${this.state.icon[1]}.png`}
               />
             </div>
             <div className="location-box"></div>
             <div className="weather-box">
               <div className="date">{dateBuilder(thirdDay)}</div>
               <div className="temp">
-                {Math.round(this.state.activeWeather3)} ‎°C{" "}
+                {Math.round(this.state.activeWeather[2])} ‎°C{" "}
               </div>
-              <div className="weather">{this.state.activeDesc3} </div>
+              <div className="weather">{this.state.sysName[2]} </div>
               <img
-                src={`http://openweathermap.org/img/wn/${this.state.icon3}.png`}
+                src={`http://openweathermap.org/img/wn/${this.state.icon[2]}.png`}
               />
             </div>
-
             <div className="location-box"></div>
             <div className="weather-box">
               <div className="date">{dateBuilder(forthDay)}</div>
               <div className="temp">
-                {Math.round(this.state.activeWeather4)} ‎°C{" "}
+                {Math.round(this.state.activeWeather[3])} ‎°C{" "}
               </div>
-              <div className="weather">{this.state.activeDesc4} </div>
+              <div className="weather">{this.state.sysName[3]} </div>
               <img
-                src={`http://openweathermap.org/img/wn/${this.state.icon4}.png`}
+                src={`http://openweathermap.org/img/wn/${this.state.icon[3]}.png`}
               />
             </div>
             <div className="location-box"></div>
             <div className="weather-box">
               <div className="date">{dateBuilder(fifthDay)}</div>
               <div className="temp">
-                {Math.round(this.state.activeWeather5)} ‎°C{" "}
+                {Math.round(this.state.activeWeather[4])} ‎°C{" "}
               </div>
-              <div className="weather">{this.state.activeDesc5} </div>
+              <div className="weather">{this.state.sysName[4]} </div>
               <img
-                src={`http://openweathermap.org/img/wn/${this.state.icon5}.png`}
+                src={`http://openweathermap.org/img/wn/${this.state.icon[4]}.png`}
               />
             </div>
           </div>
@@ -148,5 +147,4 @@ class Day extends React.Component {
     );
   }
 }
-
 export default Day;
